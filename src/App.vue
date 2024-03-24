90b91a6cded4aac7a065b4e96eff53d8c8120f1a
sxr133 <96782879+sxr133@users.noreply.github.com>
2024-03-23 20:22:26 -0400

    tailwind error

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
@@ -14,13 +14,4 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./assets/main.css"/>
